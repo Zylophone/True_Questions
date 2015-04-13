1addd39158d2060155470b4177268f9ed988205c
lixiangcliff <lixiang.cliff@gmail.com>
2015-04-13 14:03:34 -0700

    add Binary_Array_Split_and_Compress

---
+++ True_Questions/src/Binary_Array_Split_and_Compress/Question.java
@@ -0,0 +1,22 @@
+package Binary_Array_Split_and_Compress;
+
+public class Question {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	/**
+	 * http://www.mitbbs.com/article_t/JobHunting/32932495.html
+	 * google onsite
+	 * 给定二维binary数组，要求分割然后压缩
+	 */
+
+	/*
+	 * 未理解题意。。
+	 */
+	
+}

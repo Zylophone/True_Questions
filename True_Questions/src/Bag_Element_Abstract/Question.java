83ab80831a1c0209fbfc20e1505270dfb3c9b344
lixiangcliff <lixiang.cliff@gmail.com>
2015-04-13 10:56:43 -0700

    add Bag_Element_Abstract

---
+++ True_Questions/src/Bag_Element_Abstract/Question.java
@@ -0,0 +1,22 @@
+package Bag_Element_Abstract;
+
+public class Question {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	/**
+	 * bag element提取
+	 * http://www.mitbbs.com/article_t/JobHunting/32932495.html
+	 */
+
+	/*
+	 * 未搞清楚题的内容, someone is guessing this one below:
+	 * http://math.stackexchange.com/questions/389140/odds-of-choosing-items-out-of-a-bag
+	 */
+}

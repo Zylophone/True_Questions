fb058fe19f135be8412c7de380053c6f8b7746af
lixiangcliff <lixiang.cliff@gmail.com>
2015-04-13 13:53:01 -0700

    add Callback_Function_Implement

---
+++ True_Questions/src/Callback_Function_Implement/Question.java
@@ -0,0 +1,27 @@
+package Callback_Function_Implement;
+
+public class Question {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	/**
+	 * Twitter 电面
+	 * callback function实现， 要求thread safe
+	 */
+	
+	/*1. implementation
+	 * (1)看第二个答案：http://stackoverflow.com/questions/9596276/how-to-explain-callbacks-in-plain-english-how-are-they-different-from-calling-o
+	 * (2)http://stackoverflow.com/questions/824234/what-is-a-callback-function
+	 * 2.thread safe
+	 * 
+	 * 2.thread safe(应该同时研究下 multithread的更多问题)
+	 * http://blog.ometer.com/2011/07/24/callbacks-synchronous-and-asynchronous/
+	 */
+
+}

eeeea6645ef6c3cac3a474b36460f28ec7f4b94e
lixiangcliff <lixiang.cliff@gmail.com>
2015-04-13 10:42:06 -0700

    add Typeahead_Search_Design

---
+++ True_Questions/src/Typeahead_Search_Design/Question.java
@@ -0,0 +1,29 @@
+package Typeahead_Search_Design;
+
+public class Question {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	/**
+	 * http://www.mitbbs.com/article_t/JobHunting/32932495.html
+	 * design面type ahead search
+	 */
+	
+	/*
+	 * 1.google instant search: http://en.wikipedia.org/wiki/Google_Search#Instant_Search
+	 * http://searchengineland.com/how-google-instant-autocomplete-suggestions-work-62592
+	 * http://googleblog.blogspot.com/2010/09/google-instant-behind-scenes.html
+	 * http://stackoverflow.com/questions/3670831/how-does-google-instant-work
+	 * 
+	 * 
+	 * 2.http://uxmag.com/articles/designing-search-as-you-type-suggestions
+	 * 3.method for facebook: https://www.facebook.com/notes/facebook-engineering/the-life-of-a-typeahead-query/389105248919
+	 */
+
+}

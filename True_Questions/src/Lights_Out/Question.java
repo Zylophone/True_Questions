1faab5c16a06cf604b47f948148909814df05278
lixiangcliff <lixiang.cliff@gmail.com>
2015-04-13 14:34:09 -0700

    add Lights_Out

---
+++ True_Questions/src/Lights_Out/Question.java
@@ -0,0 +1,24 @@
+package Lights_Out;
+
+public class Question {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	/**
+	 * twitter onsite
+	 * http://www.mitbbs.com/article_t/JobHunting/32932495.html
+	 *  Lights Out 
+	 */
+	
+	/*
+	 * 猜不出问题是什么，找到一道智力题：
+	 * http://www.programmerinterview.com/index.php/puzzles/3-switches-3-bulbs/
+	 */
+
+}

d5f47ec33970fa80d2e419dcd6d2c48026214416
lixiangcliff <lixiang.cliff@gmail.com>
2015-04-13 14:05:27 -0700

    add Binary_Stream_Parsing

---
+++ True_Questions/src/Binary_Stream_Parsing/Question.java
@@ -0,0 +1,23 @@
+package Binary_Stream_Parsing;
+
+public class Question {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	/**
+	 * http://www.mitbbs.com/article_t/JobHunting/32932495.html
+	 * twitter电面
+	 * 给一个binary stream，分析并写出parsing函数
+	 */
+	
+	/*
+	 * 题意未懂
+	 */
+
+}

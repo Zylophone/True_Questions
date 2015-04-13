706d18ce0b31d54fdb093ad40446b8f090f12217
lixiangcliff <lixiang.cliff@gmail.com>
2015-04-13 14:26:22 -0700

    add Max_Product_in_Array

---
+++ True_Questions/src/Max_Product_in_Array/Question.java
@@ -0,0 +1,24 @@
+package Max_Product_in_Array;
+
+public class Question {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	/**
+	 * twitter onsite:
+	 * 数组除掉下标对应的元素以外的所有乘积
+	 */
+	
+	/*
+	 * 题目真难猜。。。
+	 * 我猜是这个，你觉得呢，大Zhe
+	 * http://bylijinnan.iteye.com/blog/1611731
+	 * 总不能是这道题吧： https://oj.leetcode.com/problems/maximum-product-subarray/
+	 */
+}
